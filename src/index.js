21474ded553d95c7ff1e9dd2ba5a9a4826f6f610
jayceeth <75772603+jayceeth@users.noreply.github.com>
2021-02-03 21:50:34 -0800

    Change weather icon

--- src/index.js
+++ src/index.js
@@ -20,19 +20,27 @@ function formatDate() {
   }
   return `${day} ${hours}:${minutes}`;
 }
+
 let changeDateTime = document.querySelector("#date");
 changeDateTime.innerHTML = formatDate(changeDateTime);
+
 function displayCurrentTemp(response){
     let tempElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#current-city");
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
     let descriptionElement = document.querySelector("#description");
+    let iconElement = document.querySelector("#icon");
     tempElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
     descriptionElement.innerHTML = response.data.weather[0].description;
+    iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "f0229aa4803b78f326fa1951e4c8d9a5";
